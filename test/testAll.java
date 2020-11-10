0de36efa996fad8d53c70447ab161b75c8539b3f
yusufjaamac <60450285+yusufjaamac@users.noreply.github.com>
2020-11-09 22:14:01 -0500

    Add files via upload

---
+++ test/testAll.java
@@ -0,0 +1,12 @@
+/*
+@author Yusuf J
+ */
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+@RunWith(Suite.class)
+@SuiteClasses({BoardTest.class,PlayerTest.class,CountryTest.class,ContinentTest.class,DiceTest.class})
+
+public class testAll {
+}
