e018e76f0e93b34f7d6ce9e67ed8cf908b02bf3e
Danish <danishirfan3@gmail.com>
2020-11-09 22:43:24 -0500

    tests added

---
+++ test/ContinentTest.java
@@ -0,0 +1,35 @@
+//@author yasin jaamac
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class ContinentTest {
+    Continent Australia = new Continent("Australia", 2);
+
+    @Test
+    public void getName() {
+        Player player  = new Player("Mo",7);
+        assertEquals("Mo",player.getName());
+
+    }
+
+    @Test
+    public void getBonusArmy(){
+        assertTrue(Australia.getBonusArmy()==2);
+    }
+
+
+    @Test
+    public void addCountry() {
+        //Created country
+        Country c = new Country("Canada");
+        //Created player
+        Player p = new Player("jaa",6);
+
+        //added country to the player p
+        p.addCountry(c);
+
+        //Check if player owns that country that was recently added
+        assertTrue(p.ownsCountry("Canada"));
+        assertFalse(p.ownsCountry("India"));
+    }
+}
