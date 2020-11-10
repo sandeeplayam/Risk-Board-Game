b08a7a20a08de88d3a1aaadd75e35cfe191e4fb6
Danish <danishirfan3@gmail.com>
2020-11-09 21:50:27 -0500

    Updated tests

--- test/testAll.java
+++ test/testAll.java
@@ -2,7 +2,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 @RunWith(Suite.class)
-@SuiteClasses({BoardTest.class,PlayerTest.class,CountryTest.class,ContinentTest.class})
+@SuiteClasses({BoardTest.class,PlayerTest.class,CountryTest.class,ContinentTest.class,DiceTest.class})
 //author Yusuf J
 public class testAll {
 }
