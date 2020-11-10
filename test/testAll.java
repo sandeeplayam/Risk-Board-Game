b80aaec4bde99103be57b9c74eaa5f4b22bc4ed9
yusufjaamac <60450285+yusufjaamac@users.noreply.github.com>
2020-11-09 22:13:10 -0500

    Delete testAll.java

--- test/testAll.java
+++
@@ -1,8 +0,0 @@
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
-import org.junit.runners.Suite.SuiteClasses;
-@RunWith(Suite.class)
-@SuiteClasses({BoardTest.class,PlayerTest.class,CountryTest.class,ContinentTest.class,DiceTest.class})
-//author Yusuf J
-public class testAll {
-}
